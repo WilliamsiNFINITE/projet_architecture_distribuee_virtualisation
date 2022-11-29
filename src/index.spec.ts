9dc5bd1cba8ad6d429f3c556d25ade1faf7c6a06
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-11-29 09:51:30 +0100

    update index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -40,13 +40,12 @@ describe('index test suite', () => {
         req.end();
     });
 
-    // it('should be a 200 response from the server', () => {
-    //     //test of the function createRequestListener with the url /api/v1/sysinfo
-    //     const requestListener = createRequestListener();
-    //     const server = createServer(requestListener);
-    //     const options = { hostname: '0.0.0.0', port: 8000, path: '/api/v1/sysinfo', method: 'GET' };
-    //     const req = http.request(options, res => { expect(res.statusCode).toBe(200);});
-    //     req.end();
-    //     server.close();
-    // });
+    it('should be a 200 response from the server', () => {
+        //test of the function createRequestListener with the url /api/v1/sysinfo
+
+        const options = { hostname: '0.0.0.0', port: 8000, path: '/api/v1/sysinfo', method: 'GET' };
+        const req = http.request(options, res => { expect(res.statusCode).toBe(200);});
+        req.end();
+
+    });
 });
